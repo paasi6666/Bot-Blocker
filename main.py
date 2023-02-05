53df6f5d8d2e4d9b56a4713480937309cfecb66a
paasi6666 <26493612+paasi6666@users.noreply.github.com>
2023-02-05 15:06:51 +0100

    Added support for chrome

--- main.py
+++ main.py
@@ -2,11 +2,29 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.firefox.service import Service as FirefoxService
 
-from webdriver_manager.firefox import GeckoDriverManager
+def chooseBrowser():
+    global driver
+    inp = input("Choose 1 for Firefox\nChoose 2 for Chrome\n\n")
+    if inp == "1":
+        print("You chose Firefox, installing driver..")
+        from selenium.webdriver.firefox.service import Service as FirefoxService
+        from webdriver_manager.firefox import GeckoDriverManager
+        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    elif inp == "2":
+        print("You chose Chrome, installing driver..")
+        from selenium.webdriver.chrome.service import Service as ChromeService
+        from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+        options = webdriver.ChromeOptions() 
+        options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=options)
+    else:
+        print("You must choose between 1 or 2")
+        return chooseBrowser()
+chooseBrowser()
+
 driver.get("https://9gag.com/login")
 input("Log into 9gag. Press ENTER after login.")
 
@@ -15,8 +33,8 @@ with open('bots.txt', 'r') as file:
         bot = bot.strip()
         driver.get('https://9gag.com/u/'+bot)
         try:
-            WebDriverWait(driver,5).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div[1]/div[2]/div[1]/div/div[2]/div/div/div/a")))
-            driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div[2]/div[1]/div/div[2]/div/div/div/a").click()
+            WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div[class='profile-more'] div[class='uikit-popup-menu'] div a[class='button']")))
+            driver.find_element(By.CSS_SELECTOR, "div[class='profile-more'] div[class='uikit-popup-menu'] div a[class='button']").click()
 
             WebDriverWait(driver,5).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div[1]/div[2]/div[1]/div/div[2]/div/div/div[2]/ul/li[3]/a")))
             blockButton = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div[2]/div[1]/div/div[2]/div/div/div[2]/ul/li[3]/a")
