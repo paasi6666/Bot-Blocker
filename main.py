b857d3bb426b4df68ca921bbccf2546c2aed6896
paasi6666 <26493612+paasi6666@users.noreply.github.com>
2023-02-05 19:52:34 +0100

    Added "Report Account" Feature

--- main.py
+++ main.py
@@ -8,21 +8,39 @@ def chooseBrowser():
     inp = input("Choose 1 for Firefox\nChoose 2 for Chrome\n\n")
     if inp == "1":
         print("You chose Firefox, installing driver..")
+        
         from selenium.webdriver.firefox.service import Service as FirefoxService
         from webdriver_manager.firefox import GeckoDriverManager
         driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+        print("\n\n\n\n")
+        askForReportAndBlock()
     elif inp == "2":
         print("You chose Chrome, installing driver..")
+
         from selenium.webdriver.chrome.service import Service as ChromeService
         from webdriver_manager.chrome import ChromeDriverManager
-
         options = webdriver.ChromeOptions() 
         options.add_experimental_option("excludeSwitches", ["enable-logging"])
-
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=options)
+        
+        print("\n\n\n\n")
+        askForReportAndBlock()
     else:
-        print("You must choose between 1 or 2")
+        print("\n\n\n\nYou must choose between 1 or 2")
         return chooseBrowser()
+
+def askForReportAndBlock():
+    global reportAndBlock
+    inp = input("Choose 1 to Block accounts\nChoose 2 to Block and Report accounts\n\n")
+    if inp == "1":
+        reportAndBlock = False
+    elif inp == "2":
+        reportAndBlock = True
+    else:
+        print("\n\n\n\nYou must choose between 1 or 2")
+        return askForReportAndBlock()
+
 chooseBrowser()
 
 driver.get("https://9gag.com/login")
@@ -33,12 +51,35 @@ with open('bots.txt', 'r') as file:
         bot = bot.strip()
         driver.get('https://9gag.com/u/'+bot)
         try:
+            if(reportAndBlock):
+                #Report User
+                #Click Menu-Button
+                WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".uikit-popup-menu a")))
+                driver.find_element(By.CSS_SELECTOR, ".uikit-popup-menu a").click()
+
+                #Click Report Button
+                WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".uikit-popup-menu .menu ul li:nth-child(2) a")))
+                driver.find_element(By.CSS_SELECTOR, ".uikit-popup-menu .menu ul li:nth-child(2) a").click()
+
+                #Click Spam Button
+                WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".report-type__type div:nth-child(2)")))
+                driver.find_element(By.CSS_SELECTOR, ".report-type__type div:nth-child(2)").click()
+
+                #Click Report Button
+                WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".report-btn")))
+                driver.find_element(By.CSS_SELECTOR, ".report-btn").click()
+            
+            #Block User
+            #Click Menu-Button
             WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".uikit-popup-menu a")))
             driver.find_element(By.CSS_SELECTOR, ".uikit-popup-menu a").click()
 
+            #Find Block-Button
             WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".uikit-popup-menu .menu ul li:nth-child(3) a")))
             blockButton = driver.find_element(By.CSS_SELECTOR, ".uikit-popup-menu .menu ul li:nth-child(3) a")
             print(blockButton.get_attribute('innerHTML'))
+
+            #Check if User is already blocked or not
             if "Unblock" not in blockButton.get_attribute('innerHTML'):
                 blockButton.click()
 
